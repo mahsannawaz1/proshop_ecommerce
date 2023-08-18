4bdfc7c07558c52418e99d2c38b71b903eb1856e
Ahsan Nawaz <nawazehsen@gmail.com>
2023-08-18 23:38:42 +0500

    Added create-order view with its urlpattern

--- backend/base/urls/order_urls.py
+++ backend/base/urls/order_urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from base.views import order_views as views
 
 urlpatterns = [
+   path('add',views.addOrderItems,name="create-order"),
    
    
 ]
--- backend/base/views/order_views.py
+++ backend/base/views/order_views.py
@@ -2,10 +2,49 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view,permission_classes
 from rest_framework.permissions import IsAdminUser,IsAuthenticated
-from base.models import Product
-from base.serializers import ProductSerializer,UserSerializer,UserSerializerWithToken
+from base.models import Product,Order,OrderItem,ShippingAddress
+from base.serializers import ProductSerializer
 from django.contrib.auth.models import User
-from django.contrib.auth.hashers import make_password
-from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
-from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework import status
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def addOrderItems(request):
+  user=request.user
+  data=request.data
+  orderItems=data['orderItems']
+  if orderItems and len(orderItems) == 0:
+    return Response({'detail':'No Order items'},status=status.HTTP_400_BAD_REQUEST)
+  else:
+    order=Order.objects.create(
+      user=user,
+      payment_method=data['paymentMethod'],
+      taxPrice=data['taxPrice'],
+      shippingPrice=data['shippingPrice'],
+      totalPrice=data['totalPrice']
+    )
+    shipping = ShippingAddress.objects.create(
+      order=order,
+      address=data['shippingAddress']['address'],
+      city=data['shippingAddress']['city'],
+      postalCode=data['shippingAddress']['postalCode'],
+      country=data['shippingAddress']['country'] 
+    )
+
+    for i in orderItems:
+      product=Porduct.objects.get(id=i['product'])
+      item=OrderItem.objects.create(
+        product=product,
+        order=order,
+        name=product.name,
+        qty=i['qty'],
+        price=i['price'],
+        image=product.image.url 
+      )
+
+      product.countInStock -=item.qty 
+      product.save()
+
+
+
+  return Response('ORDER')
