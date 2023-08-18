ab801620209f28d06545a3f0e32a68a2574f81f6
Ahsan Nawaz <nawazehsen@gmail.com>
2023-08-18 23:10:19 +0500

    Added the frontend page for placing order

---
+++ frontend/src/screens/PlaceOrderScreen.jsx
@@ -0,0 +1,135 @@
+import React, { useState, useEffect } from 'react';
+import {useDispatch,useSelector} from 'react-redux'
+import { Row,Col,ListGroup,Image,Card, Button } from 'react-bootstrap';
+import { Link, useNavigate } from 'react-router-dom';
+import FormContainer from '../components/common/FormContainer';
+import CheckoutSteps from '../components/CheckoutSteps';
+import Message from './../components/Message';
+
+
+function PlaceOrderScreen() {
+  const cart = useSelector(state => state.cart)
+  const { cartItems, shippingAddress, paymentMethod } = cart
+  let itemsPrice=0
+  for (var i = 0; i < cartItems.length; i++) { 
+      itemsPrice += Number(cartItems[i].qty * cartItems[i].price)
+  }
+  
+  let shippingPrice = ((itemsPrice < 100) ? 10 : 0)
+  let taxPrice = (itemsPrice * 0.25)
+  let totalPrice = (itemsPrice + shippingPrice + taxPrice)
+  
+  
+  const placeOrderHandler = () => {
+    console.log('order placed')
+  }
+  return ( 
+    <div>
+      <CheckoutSteps step1 step2 step3 step4 />
+      <Row>
+        <Col md={8}>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <h2>Shipping</h2>
+              <p>
+                <strong>
+                  Shipping: {shippingAddress.address}, {shippingAddress.city}
+                  {'   '}
+                  {shippingAddress.postalCode},
+                  {'   '}
+                  {shippingAddress.country}
+                </strong>
+              </p>
+            </ListGroup.Item>
+            
+            <ListGroup.Item>
+              <h2>Payment Method</h2>
+              <p>
+                <strong>
+                  Method: {paymentMethod}
+                </strong>
+              </p>
+            </ListGroup.Item>
+            
+            <ListGroup.Item>
+              <h2>Order Items</h2>
+              {
+                cartItems.length === 0 ? <Message variant="info">Your cart is empty</Message> :
+                  (
+                    <ListGroup variant="flush">
+                      {cartItems.map((item, index) => (
+                        <ListGroup.Item key={index}>
+                          <Row>
+                            <Col md={1}>
+                              <Image src={item.image} alt={item.name} fluid rounded />
+                            </Col>
+                            <Col>
+                              <Link to={`/products/${item.product}`}>{item.name}</Link>
+                            </Col>
+                            <Col md={4}>
+                              {item.qty}x ${item.price} = ${(item.qty * item.price).toFixed(2)}
+                            </Col>
+                          </Row>
+                        </ListGroup.Item>
+                      ))}
+
+                    </ListGroup>
+                )
+              }
+              </ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col md={4}>
+          <Card>
+            <ListGroup variant="flush">
+
+              <ListGroup.Item>
+                <h2>Order Summary</h2>
+              </ListGroup.Item>
+
+              <ListGroup.Item>
+                <Row>
+                  <Col>Item:</Col>
+                  <Col>${ itemsPrice.toFixed(2)  }</Col>
+                </Row>
+              </ListGroup.Item>
+
+              <ListGroup.Item>
+                <Row>
+                  <Col>Shipping:</Col>
+                  <Col>${ shippingPrice.toFixed(2)   }</Col>
+                </Row>
+              </ListGroup.Item>
+
+              <ListGroup.Item>
+                <Row>
+                  <Col>Tax:</Col>
+                  <Col>${ taxPrice.toFixed(2)   }</Col>
+                </Row>
+              </ListGroup.Item>
+
+              <ListGroup.Item>
+                <Row>
+                  <Col>Total:</Col>
+                  <Col>${ totalPrice.toFixed(2)  }</Col>
+                </Row>
+              </ListGroup.Item>
+              
+              <ListGroup.Item>
+
+              </ListGroup.Item>
+              <Button
+                type="button"
+                className="btn-block"
+                disabled={cartItems.length === 0}
+                onClick={placeOrderHandler}
+              >Place Order</Button>
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
+    </div>
+   );
+}
+
+export default PlaceOrderScreen;
